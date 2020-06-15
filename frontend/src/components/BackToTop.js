7e5cdbc1fa1f3ed87cadbe3553b211e3a63569e7
casdon_mystery <pascalemy2010@gmail.com>
2020-06-15 15:58:50 +0100

    added some features

--- frontend/src/components/BackToTop.js
+++ frontend/src/components/BackToTop.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { storeContext } from '../STATES/Actions/Actions';
 
@@ -17,19 +17,11 @@ const backToTopStyle = {
 const BackToTop = () => {
 
     const { storestate } = useContext(storeContext);
-    const [backTotop, setbackTotop] = useState({ back: false })
     const { scrow } = storestate
     const onClick = () => {
         window.scrollTo("", 0)
-        setbackTotop({ back: !backTotop.back })
     }
-    useEffect(() => {
-        // window.scrollTop = 0
-        console.log("helo")
-    }, [backTotop])
     let display = "none"
-
-
 
     if (scrow >= 1000) {
         display = "block"
