69da3a6f6440b13def2a8addfb328981360e0a2e
casdon_mystery <pascalemy2010@gmail.com>
2020-06-20 22:27:46 +0100

    made changes

--- ecommerces/settings.py
+++ ecommerces/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY_2")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.251','127.0.0.1']
+ALLOWED_HOSTS = ["castechapps.herokuapp.com"]
 
 
 # Application definition
